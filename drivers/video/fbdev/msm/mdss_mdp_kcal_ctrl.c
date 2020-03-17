3bff480cf50b01fc041e0d308087da98cb965a62
bdashore3 <bdashore3@gmail.com>
2020-03-17 02:46:09 +0200

    kcal & mdss: Add hooks for K-Lapse to work
    These lines of code are enabled only when K-Lapse is enabled in defconfig
    
    Co-authored-by: Tanish Manku <tanish2k09.dev@gmail.com>
    Co-authored-by: kingbri <bdashore3@gmail.com>

--- drivers/video/fbdev/msm/mdss_mdp_kcal_ctrl.c
+++ drivers/video/fbdev/msm/mdss_mdp_kcal_ctrl.c
@@ -61,6 +61,10 @@ struct kcal_lut_data {
 	int cont;
 };
 
+#ifdef CONFIG_KLAPSE
+struct kcal_lut_data *lut_cpy;
+#endif
+
 struct mdss_mdp_ctl *fb0_ctl = 0;
 
 static int mdss_mdp_kcal_store_fb0_ctl(void)
@@ -531,6 +535,31 @@ static DEVICE_ATTR(kcal_val, S_IWUSR | S_IRUGO, kcal_val_show, kcal_val_store);
 static DEVICE_ATTR(kcal_cont, S_IWUSR | S_IRUGO, kcal_cont_show,
 	kcal_cont_store);
 
+#ifdef CONFIG_KLAPSE
+void klapse_kcal_push(int r, int g, int b)
+{
+  lut_cpy->red = r;
+	lut_cpy->green = g;
+	lut_cpy->blue = b;
+
+	mdss_mdp_kcal_update_pcc(lut_cpy);
+}
+
+/* kcal_get_color() :
+ * @param : 0 = red; 1 = green; 2 = blue;
+ * @return : Value of color corresponding to @param, or 0 if not found
+ */
+unsigned short kcal_get_color(unsigned short int code)
+{
+	if (code == 1)
+		return lut_cpy->green;
+	else if (code == 2)
+		return lut_cpy->blue;
+	else
+		return lut_cpy->red;
+}
+#endif
+
 static int mdss_mdp_kcal_update_queue(struct device *dev)
 {
 	struct kcal_lut_data *lut_data = dev_get_drvdata(dev);
@@ -611,6 +640,9 @@ static int kcal_ctrl_probe(struct platform_device *pdev)
 	}
 #endif
 
+#ifdef CONFIG_KLAPSE
+	lut_cpy = lut_data;
+#endif
 	ret = device_create_file(&pdev->dev, &dev_attr_kcal);
 	ret |= device_create_file(&pdev->dev, &dev_attr_kcal_min);
 	ret |= device_create_file(&pdev->dev, &dev_attr_kcal_enable);
